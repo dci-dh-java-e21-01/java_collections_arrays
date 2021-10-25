b0ba29673fc3f769fa0f57097d08100aa512fda0
Hadi Nsreeny <39328879+hnsreeny@users.noreply.github.com>
2021-10-25 11:01:18 +0200

    comments

--- Main.java
+++ Main.java
@@ -10,14 +10,14 @@ public class Main {
         // The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.
         int age = 32;
         System.out.println(age);
-        //
+        // One-dimensional
         String[] names = {"Ali","Olga","Zain","Nancy"};
         System.out.println(names[0]);
         String[] cars = {"BMW","Ford","Audi"};
         System.out.println(cars[2]);
         cars[1] = "Bugatti";
         System.out.println(cars[1]);
-        //
+        // Multi-dimensional
         int[][] numbers = {{1,2,3,4},{5,6,7,8}};
         System.out.println(Arrays.toString(numbers[0]));
     }
